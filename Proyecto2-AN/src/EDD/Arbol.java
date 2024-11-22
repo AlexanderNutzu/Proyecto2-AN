2ca51f4e56824ff67d2f03022c307d7791f827ee
AlexanderNutzu <alexander.nutzu@correo.unimet.edu.ve>
2024-11-22 08:08:09 -0400

    Progreso EDDs

--- Proyecto2-AN/src/EDD/Arbol.java
+++ Proyecto2-AN/src/EDD/Arbol.java
@@ -4,10 +4,172 @@
  */
 package EDD;
 
+import ClasesPrincipales.Persona;
+
 /**
  *
  * @author alexa
  */
 public class Arbol {
+    private NodoA root;
+
+    public Arbol() {
+        this.root = null;
+    }
+
+    public NodoA getRoot() {
+        return root;
+    }
+
+    public void setRoot(NodoA root) {
+        this.root = root;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public void iniciarlizarRaiz(Object dato) {
+        NodoA rootNueva = new NodoA(dato);
+        this.setRoot(rootNueva);
+    }
+
+    public void insertar(NodoA padre, Object dato) {
+        NodoA hijo = new NodoA(dato);
+        hijo.setPadre(padre);
+        padre.aggHijo(hijo);
+    }
+
+    public NodoA buscarNombreUnico(String nombreUnico) {
+        if (!this.isEmpty()) {
+            Cola cola = new Cola();
+
+            cola.enColar(this.root);
+
+            while (!cola.colaVacia()) {
+                NodoA nodoActual = (NodoA) cola.desEnColar();
+                Persona personaActual = (Persona) nodoActual.getDato();
+
+                if (personaActual.getNombreUnico().equalsIgnoreCase(nombreUnico) || personaActual.getNumeral().equalsIgnoreCase(nombreUnico)) {
+                    return nodoActual;
+                }
+
+                Nodo temp = nodoActual.getHijos().getpFirst();
+                while (temp != null) {
+                    NodoA nodoHijoActual = (NodoA) temp.getDato();
+                    cola.enColar(nodoHijoActual);
+
+                    temp = temp.getPnext();
+                }
+            }
+
+        }
+        return null;
+    }
+
+    public void mostrarPorNiveles() {
+        if (!this.isEmpty()) {
+            String arbolStr = "Arbol General Por Niveles:\n";
+            Cola cola = new Cola();
+
+            cola.enColar(this.root);
+
+            while (!cola.colaVacia()) {
+                NodoA nodoActual = (NodoA) cola.desEnColar();
+                Persona personaActual = (Persona) nodoActual.getDato();
+
+                arbolStr += personaActual.toString() + "\n";
+
+                Nodo temp = nodoActual.getHijos().getpFirst();
+                while (temp != null) {
+                    NodoA nodoHijoActual = (NodoA) temp.getDato();
+                    cola.enColar(nodoHijoActual);
+
+                    temp = temp.getPnext();
+                }
+            }
+
+            System.out.println(arbolStr);
+        }
+    }
+
+    public int maximoNivel() {
+        if (this.isEmpty()) {
+            return 0;
+        }
+        int max = 0;
+        Cola cola = new Cola();
+        cola.enColar(this.root);
+
+        Cola colaNiveles = new Cola();
+        colaNiveles.enColar(1);
+
+        while (!cola.colaVacia()) {
+            NodoA nodoActual = (NodoA) cola.desEnColar();
+            int nivelActual = (int) colaNiveles.desEnColar();
+
+            max = Math.max(max, nivelActual);
+
+            Nodo temp = nodoActual.getHijos().getpFirst();
+            while (temp != null) {
+                NodoA nodoHijoActual = (NodoA) temp.getDato();
+                cola.enColar(nodoHijoActual);
+                colaNiveles.enColar(nivelActual + 1);
+                temp = temp.getPnext();
+            }
+        }
+
+        return max;
+    }
+
+    public Lista listaNivel(int nivel) {
+        Lista personasNivel = new Lista();
+        if (this.isEmpty()) {
+            return null;
+        }
+        
+        Cola cola = new Cola();
+        cola.enColar(this.root);
+
+        Cola colaNiveles = new Cola();
+        colaNiveles.enColar(1);
+
+        while (!cola.colaVacia()) {
+            NodoA nodoActual = (NodoA) cola.desEnColar();
+            int nivelActual = (int) colaNiveles.desEnColar();
+
+            if(nivelActual == nivel){
+                Persona personaActual = (Persona) nodoActual.getDato();
+                personasNivel.insertarFinal(personaActual);
+            }
+
+            Nodo temp = nodoActual.getHijos().getpFirst();
+            while (temp != null) {
+                NodoA nodoHijoActual = (NodoA) temp.getDato();
+                cola.enColar(nodoHijoActual);
+                colaNiveles.enColar(nivelActual + 1);
+                temp = temp.getPnext();
+            }
+        }
+
+        return personasNivel;
+
+    }
     
+    public Lista buscarAntepasados(NodoA nodo){
+        if(nodo == null){
+            return null;
+        }
+        
+        Lista ancestros = new Lista();
+        NodoA actual = nodo.getPadre();
+        
+        while(actual != null){
+            Persona personaActual = (Persona) actual.getDato();
+            ancestros.insertarFinal(personaActual);
+            actual = actual.getPadre();
+        }
+        
+        return ancestros;
+    }
 }
--- Proyecto2-AN/src/EDD/NodoA.java
+++ Proyecto2-AN/src/EDD/NodoA.java
@@ -11,5 +11,64 @@ import ClasesPrincipales.Persona;
  * @author alexa
  */
 public class NodoA {
+    private Object dato;
+    private NodoA padre;
+    private Lista hijos;
+
+    public NodoA(Object dato) {
+        this.dato = dato;
+        this.padre = null;
+        this.hijos = new Lista();
+    }
+
+    public Object getDato() {
+        return dato;
+    }
+
+    public void setDato(Object dato) {
+        this.dato = dato;
+    }
+
+    public NodoA getPadre() {
+        return padre;
+    }
+
+    public void setPadre(NodoA padre) {
+        this.padre = padre;
+    }
+
+    public Lista getHijos() {
+        return hijos;
+    }
+
+    public void setHijos(Lista hijos) {
+        this.hijos = hijos;
+    }
     
+    public void aggHijo(NodoA hijo){
+        if(!this.buscarHijo(hijo)){
+            this.hijos.insertarFinal(hijo);
+        }
+    }
+
+    public boolean buscarHijo(NodoA hijo) {
+        if (!this.esHoja()) {
+            Persona personaNueva = (Persona) hijo.getDato();
+            for (int i = 0; i < this.hijos.getSize(); i++) {
+                NodoA hijoActual = (NodoA) this.hijos.getValor(i);
+                Persona personaActual = (Persona) hijoActual.getDato();
+                if (personaActual.getNombreUnico().equalsIgnoreCase(personaNueva.getNombreUnico())) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        
+        return false;
+    }
+
+    public boolean esHoja() {
+        return this.hijos.isEmpty();
+    }
 }
