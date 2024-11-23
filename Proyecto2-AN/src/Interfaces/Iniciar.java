cc60efb039dfcb6e3c7a5c5dfe924b77a62169ad
AlexanderNutzu <alexander.nutzu@correo.unimet.edu.ve>
2024-11-23 12:47:17 -0400

    Iniciar :100:

--- Proyecto2-AN/src/Interfaces/Iniciar.java
+++ Proyecto2-AN/src/Interfaces/Iniciar.java
@@ -4,17 +4,27 @@
  */
 package Interfaces;
 
+import CargarArchivos.Cargar;
+import CargarArchivos.FileChooser;
+import ClasesPrincipales.GestionApp;
+import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author alexa
  */
 public class Iniciar extends javax.swing.JFrame {
 
-    /**
-     * Creates new form Iniciar
-     */
+    public static GestionApp gestionApp = new GestionApp();
+    
     public Iniciar() {
         initComponents();
+        this.setVisible(true);
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
     }
 
     /**
@@ -26,21 +36,68 @@ public class Iniciar extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        empezar = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI Black", 0, 36)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Bienvenidos");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 60, 400, -1));
+
+        empezar.setText("Empezar");
+        empezar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                empezarActionPerformed(evt);
+            }
+        });
+        jPanel1.add(empezar, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 150, 160, 30));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 300, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void empezarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_empezarActionPerformed
+        Cargar cargarArchivo = new Cargar();
+        FileChooser abrir = new FileChooser(this);
+        
+        String ruta = abrir.abrirArchivo();
+        
+        try {
+            cargarArchivo.cargarArchivo(ruta);
+            if(cargarArchivo.getEstructuraArbol()!=null){
+                JOptionPane.showMessageDialog(null, "Carga exitosa");
+                //cargarArchivo.getEstructuraArbol().mostrarPorNiveles();
+                
+                //Iniciar Gestion App
+                gestionApp.setArbolGenealogico(cargarArchivo.getEstructuraArbol());
+                gestionApp.setTablaPersonas(cargarArchivo.getTablaHash());
+                gestionApp.setNombreFamilia(cargarArchivo.getNombreFamilia());
+                
+                gestionApp.getTablaPersonas().mostrarTabla();
+                Menu menu = new Menu();
+                this.dispose();
+            }else{
+                JOptionPane.showMessageDialog(null, "Hay errores en el JSON");
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Iniciar.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_empezarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +135,8 @@ public class Iniciar extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton empezar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
